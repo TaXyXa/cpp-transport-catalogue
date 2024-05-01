65157d6842964c812ab7c721b9a23b6514b4ffec
TaXyXa <AAGolo@yandex.ru>
2024-05-01 12:51:58 +0500

    Sprint 12. Review 1 and correct main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -10,17 +10,17 @@ using namespace std;
 
 int main() {
     ///*
-    std::ifstream input;
-    std::ofstream output;
-    input.open("intizm.txt");
-    output.open("out.txt");
+    //std::ifstream input;
+    //std::ofstream output;
+    //input.open("input.txt");
+    //output.open("out.txt");
     TransportCatalogue catalogue;
     renderer::MapRenderer map_renderer;
     TransportRouter router(catalogue);
     RequestHandler request_handler(catalogue, map_renderer, router);
     json::Reader json_reader(request_handler);
-    json_reader.SetInfo(input);
-    json_reader.PrintInfo(output);
+    json_reader.SetInfo(cin);
+    json_reader.PrintInfo(cout);
     //svg::Document doc;
     //request_handler.RenderMap(doc);
     //doc.Render(output);
